a3d43142561fcdba7752b54ba45929ff144d0f01
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2023-10-09 17:09:09 +0530

    Update BookDetailServiceImpl.java

--- src/main/java/org/sahan/service/impl/BookDetailServiceImpl.java
+++ src/main/java/org/sahan/service/impl/BookDetailServiceImpl.java
@@ -1,7 +1,10 @@
 package org.sahan.service.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
+import org.sahan.dto.AddressDTO;
 import org.sahan.dto.BookDetailDTO;
 import org.sahan.entity.BookDetail;
 import org.sahan.repo.BookDetailRepo;
@@ -9,6 +12,7 @@ import org.sahan.service.BookDetailService;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +27,58 @@ public class BookDetailServiceImpl implements BookDetailService {
         this.repo = repo;
         this.mapper = mapper;
     }
+
+    String jsonString = "[\n" +
+            "  {\n" +
+            "    \"date\": \"May 09, 2023\",\n" +
+            "    \"toAddress\": \"Ms. Thamali Rodrigo\\nPartner, KPMG, P.O. Box 186,\\nNo:32, Sir Mohamed Macan Markar Mawatha,\\nColombo 03\",\n" +
+            "    \"clientName\": \"SENKADAGALA FINANCE PLC\",\n" +
+            "    \"clientNumber\": \"1024712-01\",\n" +
+            "    \"funds\": [\n" +
+            "      {\n" +
+            "        \"fundName\": \"NDB WEALTH MONEY FUND\",\n" +
+            "        \"NoOfUnits\": 0,\n" +
+            "        \"unitPrice\": 0,\n" +
+            "        \"marketValue\": 0,\n" +
+            "        \"gains\": 0\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"fundName\": \"NDB WEALTH MONEY FUND\",\n" +
+            "        \"NoOfUnits\": 0,\n" +
+            "        \"unitPrice\": 0,\n" +
+            "        \"marketValue\": 0,\n" +
+            "        \"gains\": 0\n" +
+            "      }\n" +
+            "    ],\n" +
+            "    \"referenceName\": \"NDB WEALTH MANAGEMENT LIMITED\",\n" +
+            "    \"ccAddress\": \"SENKADAGALA FINANCE PLC\\nNO 267, GALLE ROAD\\nCOLOMBO 03\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"date\": \"May 09, 2023\",\n" +
+            "    \"toAddress\": \"Mr.Sahan Nimesha\\nPartner, KPMG, P.O. Box 186,\\nNo:32, Sir Mohamed Macan Markar Mawatha,\\nColombo 03\",\n" +
+            "    \"clientName\": \"SAHAN NIMESHA PLC\",\n" +
+            "    \"clientNumber\": \"199999\",\n" +
+            "    \"funds\": [\n" +
+            "      {\n" +
+            "        \"fundName\": \"NDB WEALTH MONEY FUND\",\n" +
+            "        \"NoOfUnits\": 0,\n" +
+            "        \"unitPrice\": 0,\n" +
+            "        \"marketValue\": 0,\n" +
+            "        \"gains\": 0\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"fundName\": \"NDB WEALTH MONEY FUND\",\n" +
+            "        \"NoOfUnits\": 0,\n" +
+            "        \"unitPrice\": 0,\n" +
+            "        \"marketValue\": 0,\n" +
+            "        \"gains\": 0\n" +
+            "      }\n" +
+            "    ],\n" +
+            "    \"referenceName\": \"NDB WEALTH MANAGEMENT LIMITED\",\n" +
+            "    \"ccAddress\": \"SENKADAGALA FINANCE PLC\\nNO 267, GALLE ROAD\\nCOLOMBO 03\"\n" +
+            "  }\n" +
+            "]";
+
 
     @Override
     public void saveBookDetail(BookDetailDTO dto) {
@@ -70,6 +126,35 @@ public class BookDetailServiceImpl implements BookDetailService {
         List<BookDetail> all = repo.findAll();
         return mapper.map(all, new TypeToken<List<BookDetailDTO>>() {
         }.getType());
+    }
+
+    @Override
+    public List<String> getAllClients() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<String> clientNumbers = new ArrayList<>();
+
+        try {
+            JsonNode jsonNode = objectMapper.readTree(jsonString);
+
+            // Assuming the JSON is an array with one or more elements
+            for (JsonNode clientNode : jsonNode) {
+                JsonNode clientNumberNode = clientNode.get("clientNumber");
+                if (clientNumberNode != null && clientNumberNode.isTextual()) {
+                    clientNumbers.add(clientNumberNode.asText());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("No customers found");
+        }
+
+        return clientNumbers;
+    }
+
+
+    @Override
+    public AddressDTO searchClient(String clientNumber) {
+        return null;
     }
 
 }
